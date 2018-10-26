45d1a52f80ed9035114e13f259087844065a3f5b
jojo0severo <joaovbsevero@gmail.com>
2018-10-26 14:50:24 -0300

    Realização de testes

--- src/testes.py
+++ src/testes.py
@@ -10,13 +10,25 @@ from functools import reduce
 # Testa compressão de lista para percorrer lista de palavras
 # No java é o equivalente à: lista.forEach(v -> func(v))
 
-def func(x):
-    if x == 'c':
-        return False
-    return True
+# def func(x):
+#     if x == 'c':
+#         return False
+#     return True
+#
+#
+# lista = ['a','b','c','d','e','f','g']
+#
+# lista = [palavra for palavra in lista if func(palavra)]
+# print(lista)
 
 
-lista = ['a','b','c','d','e','f','g']
+saida1 = open('../arquivos/saida_teste1', 'r').read().split('\n')
+saida2 = open('../arquivos/saida_teste2', 'r').read().split('\n')
 
-lista = [palavra for palavra in lista if func(palavra)]
-print(lista)
+saida1.sort()
+saida2.sort()
+
+if saida1 == saida2:
+    print('Eh verdade esse bilete')
+else:
+    print('ERROOOOWW')
